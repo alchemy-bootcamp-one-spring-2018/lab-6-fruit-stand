1483eab71b2b6d0be5124ee0a122989ee8bc1355
Kevin Grayson <k.grayson.ms@gmail.com>
2018-05-08 14:48:19 -0700

    Changed fruit.js to reflect individual objects

--- js/app.js
+++ js/app.js
@@ -3,14 +3,13 @@
 
 
 function createLi() {
-    for(var i = 0; i < fruits.length; i++); {
-        var newEl = document.createElement('li');
-        var newText = document.createTextNode(fruits[i]);
-        newEl.appendChild(newText);
-        var position = document.getElementById('fruit-list')[0];
-        position.appendChild(newEl);
-    }
+    //for(var i = 0; i < fruits.length; i++); {
+    var newEl = document.createElement('li');
+    var newText = document.createTextNode(fruits[1]);
+    newEl.appendChild(newText);
+    var position = document.getElementById('fruit-list');
+    position.appendChild(newEl);
+    //}
 }
 
-console.log(fruits);
 createLi();
--- js/fruit.js
+++ js/fruit.js
@@ -10,92 +10,113 @@ size: ['small', 'medium', 'large']
 howMany: number
 */
 
+/* const testFruit = [
+    {
+        apple: {
+            
+        }
+    }
+] */
+
+
+
 const fruits = [
     {
-        name: 'avocado',
-        color: 'green',
-        isFavorite: true,
-        taste: 'sweet',
-        size: 'medium',
-        howMany: 1,
+        apple: {
+            color: 'green',
+            isFavorite: true,
+            taste: 'sweet',
+            size: 'medium',
+            howMany: 1,
+        }
+    },
+    
+    {
+        banana: {
+            color: 'yellow',
+            isFavorite: true,
+            taste: 'sweet',
+            size: 'medium',
+            howMany: 1,
+        }
     },
 
     {
-        name: 'banana',
-        color: 'yellow',
-        isFavorite: true,
-        taste: 'sweet',
-        size: 'medium',
-        howMany: 1,
+        cherry: {
+            color: 'red',
+            isFavorite: false,
+            taste: 'tart',
+            size: 'small',
+            howMany: 1,
+        }
     },
 
     {
-        name: 'cherry',
-        color: 'red',
-        isFavorite: false,
-        taste: 'tart',
-        size: 'small',
-        howMany: 1,
-    },
-    {
-        name: 'grape',
-        color: 'purple',
-        isFavorite: false,
-        taste: 'sweet',
-        size: 'small',
-        howMany: 1,
+        grape: {
+            color: 'purple',
+            isFavorite: false,
+            taste: 'sweet',
+            size: 'small',
+            howMany: 1,
+        }
     },
 
     {
-        name: 'green apple',
-        color: 'green',
-        isFavorite: false,
-        taste: 'sour',
-        size: 'medium',
-        howMany: 1,
+        greenApple: {
+            color: 'green',
+            isFavorite: false,
+            taste: 'sour',
+            size: 'medium',
+            howMany: 1,
+        }
     },
 
     {
-        name: 'kiwi',
-        color: 'green',
-        isFavorite: false,
-        taste: 'tart',
-        size: 'small',
-        howMany: 1,
+        kiwi: {
+            color: 'green',
+            isFavorite: false,
+            taste: 'tart',
+            size: 'small',
+            howMany: 1,
+        }
     },
 
     {
-        name: 'lemon',
-        color: 'yellow',
-        isFavorite: false,
-        taste: 'sour',
-        size: 'medium',
-        howMany: 1,
+        lemon: {
+            color: 'yellow',
+            isFavorite: false,
+            taste: 'sour',
+            size: 'medium',
+            howMany: 1,
+        }
     },
     {
-        name: 'orange',
-        color: 'orange',
-        isFavorite: false,
-        taste: 'sweet',
-        size: 'medium',
-        howMany: 1,
-    },
-
-    {
-        name: 'pineapple',
-        color: 'yellow',
-        isFavorite: true,
-        taste: 'tart',
-        size: 'large',
-        howMany: 1,
+        orange: {
+            color: 'orange',
+            isFavorite: false,
+            taste: 'sweet',
+            size: 'medium',
+            howMany: 1,
+        }
     },
 
     {
-        name: 'red apple',
-        color: 'red',
-        isFavorite: false,
-        taste: 'sweet',
-        size: 'medium',
-        howMany: 1,
+        pineapple: {
+            color: 'yellow',
+            isFavorite: true,
+            taste: 'tart',
+            size: 'large',
+            howMany: 1,
+        }
+    },
+
+    {
+        redApple: {
+            color: 'red',
+            isFavorite: false,
+            taste: 'sweet',
+            size: 'medium',
+            howMany: 1,
+        }
     },
 ];
