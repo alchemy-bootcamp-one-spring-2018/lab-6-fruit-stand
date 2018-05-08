e50f55a4ec77c5f0baca2dcd4cd9510c7f46ea74
MariahAdams <mariah.adams23@yahoo.com>
2018-05-08 14:05:16 -0700

    add pseudo code and images

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
-/* */
-/* */
+/* globals */
+/* exported */
 'use strict';
 
 // 1. In `app.js`:
 // 1. "import" (`globals`) `fruits` from `fruit.js`
 // 1. process the fruits, using createLi fn to Append Child to `<ul>` element 
+
+
+//loop through fruits from fruits.js
+//call createLi to append child to <ul id="fruits-list">
--- fruits.js
+++ fruits.js
@@ -1,8 +1,46 @@
-/* */
-/* */
+/* globals fruits */
+/* exported fruits */
 'use strict';
 
 
 // 1. In 'fruit.js':
 //     1. create an array of objects to define your fruit
 //     1. add function 'createLi' to each fruit that creates a `<li>` element with text
+
+//array of fruit objects
+/* fruit object will need fruitName, fruitImg, fruitPrice */
+let fruits = [
+    {
+        // fruitImg: ,
+        fruitName: 'Apples',
+        fruitPrice: '$1'
+    },
+    {
+        //fruitImg: ,
+        fruitName: 'Oranges',
+        fruitPrice: '$2'
+    },
+    {
+        //fruitImg: ,
+        fruitName: 'Banana',
+        fruitPrice: '$1'
+    },
+    {
+        //fruitImg: ,
+        fruitName: 'Pear',
+        fruitPrice: '$.50'
+    }
+];
+
+//function creatLi()
+function createLi() {
+    //code block to create <li>
+}
+
+//follow steps to add to DOM
+//make a js reference to the parent element
+//create the child element we want to add and save as a var
+//give child content (content can be text)
+//append child to the parent
+
+//function for creating ridges on background 
