656998b606c4a276ccdecc1a57b4921dedfb8453
Kevin Grayson <k.grayson.ms@gmail.com>
2018-05-08 11:34:54 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+
+// ternary
+const listID = book.hasRead ? 'read' : 'to-read';
+
+// is equal to
+let listID;
+if(book.hasRead) {
+    listId = 'read';
+}
+else {
+    listId = "to-read";
+}
