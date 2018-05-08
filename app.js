bc5c86c8e39868d87cc2390c9393177b1dc1ea77
injoongyoon <27440940+AttilaTheHen@users.noreply.github.com>
2018-05-08 13:51:58 -0700

    Implement createImg function for getting images for each fruit object

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ function renderFruit() {
 
     for(var i = 0; i < fruits.length; i++) {
         const li = fruits[i].createLi();
+        const img = fruits[i].createImg();
         list.appendChild(li);
+        list.appendChild(img);
     }
 }
--- fruit.js
+++ fruit.js
@@ -4,46 +4,76 @@ const fruits = [
     {
         name: 'apple',
         color: 'red',
+        image: 'images/apple.jpg',
         createLi: function() {
             let li = document.createElement('li');
             li.textContent = this.name + ' is ' + this.color;
             return li;
+        },
+        createImg: function() {
+            let img = document.createElement('img');
+            img.setAttribute('src', this.image);
+            return img;
         }
     },
     {
         name: 'banana',
         color: 'yellow',
+        image: 'images/banana.jpg',
         createLi: function() {
             let li = document.createElement('li');
             li.textContent = this.name + ' is ' + this.color;
             return li;
+        },
+        createImg: function() {
+            let img = document.createElement('img');
+            img.setAttribute('src', this.image);
+            return img;
         }
     },
     {
         name: 'lemon',
         color: 'yellow',
+        image: 'images/lemon.jpg',
         createLi: function() {
             let li = document.createElement('li');
             li.textContent = this.name + ' is ' + this.color;
             return li;
+        },
+        createImg: function() {
+            let img = document.createElement('img');
+            img.setAttribute('src', this.image);
+            return img;
         }
     },
     {
         name: 'orange',
         color: 'orange',
+        image: 'images/orange.jpg',
         createLi: function() {
             let li = document.createElement('li');
             li.textContent = this.name + ' is ' + this.color;
             return li;
+        },
+        createImg: function() {
+            let img = document.createElement('img');
+            img.setAttribute('src', this.image);
+            return img;
         }
     },
     {
         name: 'strawberry',
         color: 'red',
+        image: 'images/strawberry.jpg',
         createLi: function() {
             let li = document.createElement('li');
             li.textContent = this.name + ' is ' + this.color;
             return li;
+        },
+        createImg: function() {
+            let img = document.createElement('img');
+            img.setAttribute('src', this.image);
+            return img;
         }
     }
 ];
