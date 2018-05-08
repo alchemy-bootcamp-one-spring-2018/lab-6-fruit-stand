43eb8c0586dfabcbff004cb98ea937a9a80447ad
MariahAdams <mariah.adams23@yahoo.com>
2018-05-08 13:36:32 -0700

    initial commit - setup of files complete

---
+++ app.js
@@ -0,0 +1,7 @@
+/* */
+/* */
+'use strict';
+
+// 1. In `app.js`:
+// 1. "import" (`globals`) `fruits` from `fruit.js`
+// 1. process the fruits, using createLi fn to Append Child to `<ul>` element 
---
+++ fruits.js
@@ -0,0 +1,8 @@
+/* */
+/* */
+'use strict';
+
+
+// 1. In 'fruit.js':
+//     1. create an array of objects to define your fruit
+//     1. add function 'createLi' to each fruit that creates a `<li>` element with text
