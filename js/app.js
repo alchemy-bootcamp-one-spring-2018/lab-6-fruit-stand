2ef02c621a7250f8e640ca86160d16de3f060093
KLPainter <KLPainter@gmail.com>
2018-05-08 16:39:54 -0700

    final linter check

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
 /* globals fruitList */
 
+// get handle to ul element
 const ulElement = document.getElementById('fruit-list');
 
+// for each item in fruit list, create a new li and append it to the parent ul
 for(var i = 0; i < fruitList.length; i++) {
     var newLi = fruitList[i].createLi();
     ulElement.appendChild(newLi);
