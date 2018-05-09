10e828fbf148f4ec42fb59afb3a95dc796bf2a2a
rbtprograms <rbt61690@gmail.com>
2018-05-08 19:56:39 -0700

    redid with object literals and now I understand everything

--- js/app.js
+++ js/app.js
@@ -1,9 +1,16 @@
 /* globals fruitStand */
 const ulElement = document.getElementById('fruit-list');
 
+// function makeFruit(){
 for(var i = 0; i < fruitStand.length; i++){
-    
+    console.log(fruitStand[i]);
     var newLi = fruitStand[i].createLi();
+
     ulElement.appendChild(newLi);
-    console.log(newLi);
 }
+// for(var i = 0; i < fruitStand.length; i++){
+    
+//     var newLi = fruitStand[i].createLi();
+//     ulElement.appendChild(newLi);
+//     console.log(newLi);
+// }
--- js/fruits.js
+++ js/fruits.js
@@ -1,23 +1,92 @@
 /*exported fruitStand */
 
-function Fruit(name, color, imageFile){
-    this.name = name;
-    this.color = color;
-    this.imageFile - imageFile;
-    this.createLi = function() {
-        var newLi = document.createElement('li');
-        var newText = document.createTextNode(this.name);
-        var newImage = document.createElement('img');
-        newImage.src = this.imageFile;
-        newLi.appendChild(newText);
-        newLi.appendChild(newImage);
-        newLi.className = this.color;
-        return newLi;
-    };
-}
+// function Fruit(name, color, imageFile){
+//     this.name = name;
+//     this.color = color;
+//     this.imageFile - imageFile;
+//     this.createLi = function() {
+//         var newLi = document.createElement('li');
+//         var newText = document.createTextNode(this.name);
+//         var newImage = document.createElement('img');
+//         newImage.src = this.imgFile;
+//         newLi.appendChild(newText);
+//         newLi.appendChild(newImage);
+//         return newLi;
+//     };
+// }
+
+// const fruitStand = [
+//     new Fruit('apple', 'red', 'images/apple.jpg'),
+//     new Fruit('banana', 'yellow', 'images/banana.jpg')
+// ];
+
+// var apple = new fruit('apple', 'red')
 
 const fruitStand = [
-    new Fruit('apple', 'red', 'images/apple.jpg'),
-    new Fruit('banana', 'yellow', 'images/banana.jpg'),
-    new Fruit('cherry', 'red', 'images/cherry.jpg')
+
+    {
+        id: 'banana',
+        color: 'yellow',
+        seeds: false,
+        image: 'images/banana.jpg',
+        createLi: function() {
+            const item = document.createElement('li');
+            item.textContent = this.id + ' is ' + this.color;
+            const img = document.createElement('img');
+            img.setAttribute('src', this.image);
+            item.appendChild(img);
+            item.className = this.color;
+            return item;
+        }
+    },
+
+    {
+        id:'apple',
+        color: 'red',
+        seeds: true,
+        image: 'images/apple.jpg',
+        createLi: function() {
+            const item = document.createElement('li');
+            item.textContent = this.id + ' is ' + this.color;
+            const img = document.createElement('img');
+            img.setAttribute('src', this.image);
+            item.appendChild(img);
+            item.className = this.color;
+            return item;
+        }
+    },
+
+    {
+        id: 'cherry',
+        color: 'red',
+        seeds: true,
+        image: 'images/cherry.jpg',
+        createLi: function(){
+            const item = document.createElement('li');
+            item.textContent = this.id + ' is ' + this.color;
+            const img = document.createElement('img');
+            img.setAttribute('src', this.image);
+            item.appendChild(img);
+            item.className = this.color;
+            return item;
+        }
+    },
+
+    {
+        id: 'orange',
+        color: 'orange',
+        seeds: true,
+        image: 'images/orange.jpg',
+        createLi: function() {
+            const item = document.createElement('li');
+            item.textContent = this.id + ' is ' + this.color;
+            const img = document.createElement('img');
+            img.setAttribute('src', this.image);
+            item.appendChild(img);
+            item.className = this.color;
+            return item;
+        }
+    }
+
+
 ];
