410a1d595f0f2155d18734b6917e5e89a31080d9
Erik Withak <ErikAanestad@gmail.com>
2018-05-08 21:51:00 -0700

    unconsolidated .js files (imported fruit.js in html head before app.js) and animated background

--- js/app.js
+++ js/app.js
@@ -1,12 +1,4 @@
-import('./fruit.js');
-
-function Fruits(color, type, image) {
-    this.color = color;
-    this.type = type;
-    this.image = image;
-}
-
-// Instantiate fruit objects
+// Instantiate new fruit objects
 var apple = new Fruits('green', 'Apple', '../images/green-apple.jpeg');
 var orange = new Fruits('orange', 'Orange', '../images/orange.png');
 var banana = new Fruits('yellow', 'Banana', '../images/banana.jpeg');
@@ -14,17 +6,19 @@ var strawberry = new Fruits('red', 'Strawberry', '../images/strawberry.jpeg');
 var watermelon = new Fruits('green', 'Watermelon', '../images/watermelon.jpeg');
 var pear = new Fruits('yellow', 'Pear', '../images/pear.jpeg');
 
-var fruitArray = [apple, orange, banana, strawberry, watermelon, pear];
+// Make array of objects so as to loop through instead of write out individually
+var fruitArray = [apple, banana, orange, pear, strawberry, watermelon];
 
+// Needed to delay appending with null values
 var temp = document.createDocumentFragment();
 for(let i in fruitArray) {
     var card = document.createElement('li');
     card.classList = 'card';
     card.style.background = fruitArray[i].color;
     card.innerHTML = '<p>' + fruitArray[i].type + '</p><img src="' + fruitArray[i].image + '"><p>' + fruitArray[i].color + '</p>';
-    console.log(card);
     temp.appendChild(card);
 }
+// Delay append to avoid errors
 setTimeout(() => {
     document.getElementById('container-main').appendChild(temp);
 }, 100);
