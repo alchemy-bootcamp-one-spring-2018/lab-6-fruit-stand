ed771a9201c0654ae5fb7c49ad72727930e28eff
Erik Withak <ErikAanestad@gmail.com>
2018-05-08 21:34:31 -0700

    consolidated all .js to one file

--- js/app.js
+++ js/app.js
@@ -1,15 +1,30 @@
 import('./fruit.js');
 
+function Fruits(color, type, image) {
+    this.color = color;
+    this.type = type;
+    this.image = image;
+}
+
 // Instantiate fruit objects
-var apple = Fruits('green', 'Apple', '../images/green-apple.jpeg');
-var orange = Fruits('orange', 'Orange', '../images/orange.png');
-var banana = Fruits('yellow', 'Banana', '../images/banana.jpeg');
-var strawberry = Fruits('red', 'Strawberry', '../images/strawberry.jpeg');
-var watermelon = Fruits('green', 'Watermelon', '../images/watermelon.jpeg');
-var pear = Fruits('yellow', 'Pear', '../images/pear.jpeg');
+var apple = new Fruits('green', 'Apple', '../images/green-apple.jpeg');
+var orange = new Fruits('orange', 'Orange', '../images/orange.png');
+var banana = new Fruits('yellow', 'Banana', '../images/banana.jpeg');
+var strawberry = new Fruits('red', 'Strawberry', '../images/strawberry.jpeg');
+var watermelon = new Fruits('green', 'Watermelon', '../images/watermelon.jpeg');
+var pear = new Fruits('yellow', 'Pear', '../images/pear.jpeg');
 
 var fruitArray = [apple, orange, banana, strawberry, watermelon, pear];
 
+var temp = document.createDocumentFragment();
 for(let i in fruitArray) {
-    
+    var card = document.createElement('li');
+    card.classList = 'card';
+    card.style.background = fruitArray[i].color;
+    card.innerHTML = '<p>' + fruitArray[i].type + '</p><img src="' + fruitArray[i].image + '"><p>' + fruitArray[i].color + '</p>';
+    console.log(card);
+    temp.appendChild(card);
 }
+setTimeout(() => {
+    document.getElementById('container-main').appendChild(temp);
+}, 100);
--- js/fruit.js
+++ js/fruit.js
@@ -3,38 +3,3 @@ function Fruits(color, type, image) {
     this.type = type;
     this.image = image;
 }
-
-
-console.log('in the fruit.js');
-
-
-var temp = document.createDocumentFragment();
-console.log(temp);
-for(let i = 0; i < fruits.length; i++) {
-    var card = document.createElement('div');
-    card.classList = 'card';
-    card.style.background = fruits[i].color;
-    card.innerHTML = '<p>' + fruits[i].type + '</p><img src="' + fruits[i].image + '"><p>' + fruits[i].color + '</p>';
-    console.log(card);
-    temp.appendChild(card);
-}
-setTimeout(() => {
-    document.getElementById('container-main').appendChild(temp);
-}, 100);
-
-
-// import('./fruit.js');
-
-// // Instantiate fruit objects
-// var apple = Fruits('green', 'Apple', '../images/green-apple.jpeg');
-// var orange = Fruits('orange', 'Orange', '../images/orange.png');
-// var banana = Fruits('yellow', 'Banana', '../images/banana.jpeg');
-// var strawberry = Fruits('red', 'Strawberry', '../images/strawberry.jpeg');
-// var watermelon = Fruits('green', 'Watermelon', '../images/watermelon.jpeg');
-// var pear = Fruits('yellow', 'Pear', '../images/pear.jpeg');
-
-// var fruitArray = [apple, orange, banana, strawberry, watermelon, pear];
-
-// for(let i in fruitArray) {
-    
-// }
