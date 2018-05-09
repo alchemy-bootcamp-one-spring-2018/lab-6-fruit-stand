078865dfb6eeee79aaf6b636a570218fd549bc84
Kevin Grayson <k.grayson.ms@gmail.com>
2018-05-09 12:11:17 -0700

    added code to pull color property from grape

--- js/app.js
+++ js/app.js
@@ -5,10 +5,10 @@
 function create() {
     for(var i = 0; i < fruits.length; i++) {
         var first = document.querySelector('#fruit-list');
-        first.style = 'color: red';
 
         var fruitName = fruits[i].createLi();
         first.appendChild(fruitName);
+        first.style.color = fruits[3].color;
 
         var fruitImage = (fruits[i].createImg());
         first.appendChild(fruitImage);
