dd17c0c8f3fe064236ef776607ab54a3fd3f195a
Rehmsy <sarahrehmer@gmail.com>
2018-05-09 08:56:07 -0700

    made some progres in app with Zach

--- js/app.js
+++ js/app.js
@@ -2,11 +2,12 @@
 /* exported makeFruit */
 
 function makeFruit() {
-    var list = document.getElementById('my-fruit');
-    var li = myFruit.createLi();
+
 
     for(var i = 0; i < myFruit.length; i++) {
-        var li = myFruit[i].createElement('li');
-        myFruit.appendChild(li);
+        console.log(myFruit[i]);
     }
 }
+makeFruit();
+
+console.log(myFruit[0].name);
